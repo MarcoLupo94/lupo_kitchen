6f8caa165208fa3ca26a83c4a9031e65dd428a87
MarcoLupo94 <lupo.marco94@gmail.com>
2022-08-04 10:55:45 +0100

    created form layout on service page , need to improve css and finish implementing the form

--- src/modules/services.js
+++ src/modules/services.js
@@ -1,5 +1,23 @@
 const services = function(){
     $('#content').empty();
-    $('#content').append('<div id="services">This are the services</div>');
+    $('#content').append(
+      '<div id="services">\
+      <div id="contact-me">\
+        <div class="p">Book your experience today or ask for any informations!\
+        I will get back to you as soon as possible.</div>\
+        <form id="form" method="post">\
+          <input type="text"></input>\
+          <label>Name</label>\
+          <input type="number"></input>\
+          <label>Number of Guests</label>\
+          <input type="date"></input>\
+          <label>Date of the event</label>\
+          <textarea>Tell me more about the event...</textarea>\
+        </form>\
+        <div class="p">\
+        <button id="submit">Book your experience</button>\
+        </div>\
+      </div>\
+      </div>');
   };
   export {services};
