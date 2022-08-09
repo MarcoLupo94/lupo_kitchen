f8a05b4c70b130d6e15f6eb474dd2230e54a3556
MarcoLupo94 <lupo.marco94@gmail.com>
2022-08-09 18:08:33 +0100

    improved css of the page, just need to add validation on form and logo

--- src/modules/services.js
+++ src/modules/services.js
@@ -6,16 +6,32 @@ const services = function(){
         <div class="p">Book your experience today or ask for any informations!\
         I will get back to you as soon as possible.</div>\
         <form id="form" method="post">\
-          <input type="text"></input>\
-          <label>Name</label>\
-          <input type="number"></input>\
-          <label>Number of Guests</label>\
-          <input type="date"></input>\
-          <label>Date of the event</label>\
-          <textarea>Tell me more about the event...</textarea>\
+        <div id="grid">\
+          <div>\
+            <label for="user_name">*Name</label>\
+            <input type="text" id="user_name" required></input>\
+          </div>\
+          <div>\
+            <label>Number of Guests</label>\
+            <input type="number"></input>\
+          </div>\
+          <div>\
+            <label for="user_email">*Email</label>\
+            <input type="email" id="user_email" required></input>\
+          </div>\
+          <div>\
+            <label for="user_phone">Phone Number</label>\
+            <input type="number" id="user_phone" ></input>\
+          </div>\
+          </div>\
+          <div id="date-form">\
+            <label>Date of the event</label>\
+            <input type="date"></input>\
+          </div>\
+          <textarea placeholder="Tell me more about the event..."></textarea>\
         </form>\
         <div class="p">\
-        <button id="submit">Book your experience</button>\
+        <button id="submit-form" type="submit" form="form" value="Submit">Book your experience</button>\
         </div>\
       </div>\
       </div>');
